ed9ae2f535006530a87f0b5cb5d9b82186c5b922
annatret <81193761+annatret@users.noreply.github.com>
2021-05-28 13:22:36 +0200

    Python scripts updates

--- permidapi.py
+++ permidapi.py
@@ -14,16 +14,37 @@ def record_matching(token,url,template,content='text/plain',data='Organization')
     headers = {'x-ag-access-token' : token,
                'Content-Type' : content,
                'x-openmatch-dataType' : data}
-    with requests.post(url, headers = headers, data = template) as request:
-        r = request.json()
-        print('Processed: ' + str(r['numProcessedRecords']) +
-              '\nMatched: \n  Total ' + str(r['matched']['total']) +
-              '\n  Excellent ' + str(r['matched']['excellent']) +
-              '\n  Good ' + str(r['matched']['good']) +
-              '\n  Possible ' + str(r['matched']['possible']) +
-              '\nUnmatched: ' + str(r['unMatched']))
-        r = pd.DataFrame([d for d in r['outputContentResponse']])[['Match OpenPermID','Input_Name']]
-        return r
+    # Sometimes the request is processed successfully, but does not return any matches due to unexpected server errors
+    # If there are no matches, the request is sent again, 3 times in total
+    r = ''
+    matched = False
+    Ntries = 0
+    while not matched and Ntries < 3:
+        Ntries += 1
+        try:
+            with requests.post(url, headers = headers, data = template) as request:
+                r = request.json()
+                if r['errorCodeMessage'] == 'Success':
+                    if r['numReceivedRecords'] != r['unMatched']:
+                        matched = True
+                        print('Processed: ' + str(r['numProcessedRecords']) +
+                              '\nMatched: \n  Total ' + str(r['matched']['total']) +
+                              '\n  Excellent ' + str(r['matched']['excellent']) +
+                              '\n  Good ' + str(r['matched']['good']) +
+                              '\n  Possible ' + str(r['matched']['possible']) +
+                              '\nUnmatched: ' + str(r['unMatched']))
+                        r = pd.DataFrame([d for d in r['outputContentResponse']])[['Match OpenPermID','Input_Name']]
+                    elif Ntries < 3:
+                        print('No matches found. Trying again...')
+                        sleep(5)
+                    else:
+                        print('No matches found. Check the template or run the cell again.')
+        except (requests.exceptions.ConnectionError, requests.exceptions.ChunkedEncodingError) as err:
+            print('Exception occurred. Suspending and trying again...')
+            sleep(30)
+            continue
+    return r
+    
     
 def entity_search(token,url,q,entity='organization',num=5):
     """
@@ -33,9 +54,20 @@ def entity_search(token,url,q,entity='organization',num=5):
                   'entitytype': entity,
                   'num': num,
                   'q': q}
-    with requests.get(url, params = parameters) as request:
-        r = request.json()['result']['organizations']['entities']
-        return r
+    r = ''
+    Ntries = 0
+    while r == '' and Ntries < 3:
+        Ntries += 1
+        try:
+            sleep(2)
+            with requests.get(url, params = parameters) as request:
+                r = request.json()['result']['organizations']
+        except (requests.exceptions.ConnectionError, requests.exceptions.ChunkedEncodingError) as err:
+            print('Exception occurred. Suspending and trying again...')
+            sleep(100)
+            continue
+    return r
+
     
 def entity_lookup(token,url,resp='json-ld'):
     """
@@ -43,7 +75,16 @@ def entity_lookup(token,url,resp='json-ld'):
     """
     parameters =  {'access-token': token,
                    'format' : resp}
-    with requests.get(url, params = parameters) as request:
-        r = request.json()
-        return r
+    r = ''
+    Ntries = 0
+    while r == '' and Ntries < 10:
+        Ntries += 1
+        try:
+            with requests.get(url, params = parameters) as request:
+                r = request.json()
+        except (requests.exceptions.ConnectionError, requests.exceptions.ChunkedEncodingError) as err:
+            print('Exception occurred. Suspending and trying again...')
+            sleep(30)
+            continue
+    return r
 
